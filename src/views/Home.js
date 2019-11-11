024af610bb130f124b50f1604028ae80a3cb5246
Christoffer Lundberg <lundbergarn2@hotmail.com>
2019-11-11 16:35:27 +0100

    updated styling - green

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 
-import './Home.module.scss';
+import styles from './Home.module.scss';
 
 class Home extends Component {
   render() {
     return(
-      <div className="page">
-      <div className="page_background"></div>
-      <div className="page__content">
+      <div className={styles.page}>
+      <div className={styles.page_background}></div>
+      <div className={styles.page__content}>
         <h1>
-          <div>F<span className="with--accent">r</span>ont-end develope<span className="with--accent">r</span></div>
-          <div style={{marginTop: '30px'}} className="home--text">Building user experience</div>
+          <div style={{ fontWeight: '700' }}>F<span className="with--accent">r</span>ont-end develope<span className="with--accent">r</span></div>
+          <div style={{marginTop: '30px'}} className="home--text">Building use<span className="with--accent">r</span> expe<span className="with--accent">r</span>ience</div>
           <div className="home--text">and</div>
-          <div className="home--text">responsive websites</div>
+          <div className="home--text"><span className="with--accent">r</span>esponsive websites</div>
         </h1>
       </div>
       </div>
